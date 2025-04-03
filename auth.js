d1753d9d9c6efbf805bdac2e0ebf8d1c635545aa
Luscalab <lucassouza.sou@gmail.com>
2025-04-03 16:31:53 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
-const clientId = 'YOUR_GITHUB_CLIENT_ID';
+const clientId = 'Ov23liQxQxjHjqn4D8ee'; // Replace with your actual Client ID
+const redirectUri = 'https://luscalab.github.io/luscalab/callback.html';
 
 document.getElementById('githubLogin').addEventListener('click', (e) => {
     e.preventDefault();
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user`;
+    const scope = 'user';
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`;
 });
