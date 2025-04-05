056e5a47bffb7bb39d7f7ed90c54a6f8fe00bff4
Luscalab <lucassouza.sou@gmail.com>
2025-04-05 18:07:40 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -66,3 +66,13 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 });
+
+// Update scroll handler
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav-floating');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
